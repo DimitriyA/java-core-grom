65ac3b833932a4cf18ae4ae933cfa51ba148628f
DimitriyA <Zhoooray070@ukr.net>
2019-01-28 17:23:02 +0200

    th commit

---
+++ noviy/src/second/Test.java
@@ -0,0 +1,10 @@
+package second;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "aa";
+        String b = "bb";
+        String c = a+b;
+        System.out.println(c);
+    }
+}
---
+++ noviy/src/third/Test.java
@@ -0,0 +1,12 @@
+package third;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "aa";
+        String b = "bb";
+        String c = a+b;
+        System.out.println(c);
+    }
+
+
+}
